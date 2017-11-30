27a07ee97d75c37d2b85bee2ad57a41271b6882c
Cecile <cecile.hilpert@gmail.com>
2017-11-30 11:28:38 +0100

    blast.ml works + add basic blastn (prokka transcripts against complete genome ecoli) to pipeline

--- app/pipeline_v1.ml
+++ app/pipeline_v1.ml
@@ -20,8 +20,9 @@ let trimmo reads1 reads2 output1 output2 =
   ]  
 *)
 
-let reads1 = input "/home/cecile/projetM2_data/data/reads1_100k.fastq" (* Read file as workflow *)
-let reads2 = input "/home/cecile/projetM2_data/data/reads2_100k.fastq"
+let reads1 = input "/home/cecile/projetM2_data/data/ERR1073432_1.fastq" (* Read file as workflow *)
+let reads2 = input "/home/cecile/projetM2_data/data/ERR1073432_2.fastq"
+let reference = input "/home/cecile/projetM2_data/ecoli.fna"
 
 let assembly = Spades.spades ~memory:4 ~pe:([reads1],[reads2]) ()
 (* Look at spades.mli in bistro github for the arguments. The function spades returns a type [`spades] directory workflow. 
@@ -30,14 +31,24 @@ let assembly = Spades.spades ~memory:4 ~pe:([reads1],[reads2]) ()
 
 let contigs = assembly/Spades.contigs (* Selector for the contigs*)
 
-let annotation = Prokka.run contigs (* Launch prokka with default arguments. See Prokka.mli to see all optionnal arguments *)
+let annotation = Prokka.run contigs (* Launch prokka with default arguments. See Prokka.mli to see all optionnal arguments*)
+
+let prokka_transcripts = input "./resultat_complet/annotation/PROKKA_11282017.ffn"(* TO DO : add selector to prokka.ml*)
+
+let dbtype = string "nucl"
+let out_blast = "results_test.blast"
+
+let blastdb = Blast.fastadb reference dbtype 
+let results_blast = Blast.blastn ~threads:2 blastdb prokka_transcripts out_blast 
+
 
 let repo = Repo.[
   [ "assembly" ] %> assembly ; 
   [ "annotation" ] %> annotation ; 
+  [ "test" ] %> results_blast ; 
 ]
 
 
 let logger = Console_logger.create () (* Show more error messages *)
 
-let () = Repo.build ~logger ~outdir:"res" ~np:2 ~mem:(`GB 4) repo;; (* Launch pipeline *)
+let () = Repo.build ~logger ~outdir:"resultat_complet" ~np:2 ~mem:(`GB 4) repo;; (* Launch pipeline *)
