f925470594f303bcbf8abbfe75690f25842ee905
Cecile <cecile.hilpert@gmail.com>
2017-12-11 11:11:03 +0100

    Add outdir argument

--- app/projetM2.ml
+++ app/projetM2.ml
@@ -1,20 +1,21 @@
 open Core;; 
 open Bistro_utils;;
 
-let pipeline_main fq1 fq2 () = 
+let pipeline_main fq1 fq2 outdir () = 
 	let logger = Console_logger.create () in (* Show more error messages *)
 	let module P = struct 
 		let fq1 = fq1 
-		let fq2 = fq2 
+		let fq2 = fq2  
 	end in 
 	let module Pipeline = Pipeline_v1.Make(P) in 
-	let () = Repo.build ~logger ~outdir:"test_gff2" ~np:2 ~mem:(`GB 4) Pipeline.repo in () (* Launch pipeline *)
+	let () = Repo.build ~logger ~outdir ~np:2 ~mem:(`GB 4) Pipeline.repo in () (* Launch pipeline *)
 
 let pipeline_spec =
   let open Command.Spec in
   empty
   +> flag "--fq1" (required file) ~doc:"PATH Path to forward reads"
   +> flag "--fq2" (required file) ~doc:"PATH Path to reverse reads"
+  +> flag "--outdir" (required string) ~doc:"PATH Path to outdir directory"
 
 let pipeline_command =
   Command.basic
