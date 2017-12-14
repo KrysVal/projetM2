b11b18aa39722ef019abb2f289a9488d2073580d
Castro-Garcia Lucia <castrogarcialucia@gmail.com>
2017-12-14 19:20:46 +0100

    modified config_VM.sh (works)

--- config_VM.sh
+++ config_VM.sh
@@ -1,10 +1,9 @@
 sudo apt update 
 sudo apt install opam
 opam init --comp=4.05.0
-echo "/home/etudiant/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true" >> .bashrc
+#echo "~/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true" >> .bashrc
 eval `opam config env`
-sudo apt-get install m4
-opam install bistro utop
+#opam install bistro utop
 opam pin add -y bistro --dev-repo
 sudo apt-get install \
     apt-transport-https \
@@ -18,11 +17,14 @@ sudo add-apt-repository \
    stable"
 sudo apt update
 sudo apt install docker-ce
-sudo usermod -aG docker etudiant
+sudo apt-get install m4
+opam update
+opam upgrade
+sudo usermod -aG docker $USER
 opam pin add ocaml-vdom git://github.com/lexifi/ocaml-vdom.git
 opam pin add bistro_server https://github.com/pveber/bistro_server.git
 opam pin add projetM2 https://github.com/KrysVal/projetM2.git
-projetM2 web
+projetM2 web --mem 4 --np 4 --port 8080 --root-dir BAAL_repo
 
 
 
