86de1683ac6ceaeccd5831aa9585e5adbb382b92
Cecile <cecile.hilpert@gmail.com>
2017-12-17 11:50:55 +0100

    Try other version of config_VM (config_VM2.sh)
    (config_VM apparently doesn't work)

---
+++ etc/config_VM2.sh
@@ -0,0 +1,24 @@
+sudo apt update 
+sudo apt install opam
+sudo apt install m4
+opam init --comp=4.05.0
+#echo "~/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true" >> .bashrc
+eval `opam config env`
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt install docker-ce
+sudo usermod -aG docker $USER
+opam pin add -y bistro --dev-repo
+opam pin add ocaml-vdom git://github.com/lexifi/ocaml-vdom.git
+opam pin add bistro_server https://github.com/pveber/bistro_server.git
+opam pin add projetM2 https://github.com/KrysVal/projetM2.git
+projetM2 web --mem 4 --np 4 --port 8080 --root-dir BAAL_repo
